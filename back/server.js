086bfb69e8094518fad9dac24b50aeaa2e6b2fb2
Franciane <franciane.penina@gmail.com>
2022-08-26 14:34:23 -0300

    feat: enter username and photo

--- back/server.js
+++ back/server.js
@@ -5,4 +5,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const user = [{ username:'',
+                avatar:'',}]
+
+const tweet = [{username:'',
+                tweet:''}]               
+
+app.post('/sign-up',(req, res) => {
+    const loginUser = req.body;
+    user.push(loginUser);
+    
+    console.log(req.body)
+    res.send('ok');
+})
+
+
 app.listen(5000, ()=>console.log('listening on port 5000'));
